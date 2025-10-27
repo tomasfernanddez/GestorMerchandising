612fbee1a93c07bc514fa1cc55e2403886d7b873
tomasfernanddez <tomasefernandez2001@hotmail.com>
2025-10-26 22:55:28 -0300

    solucion

---
+++ DAL/Migrations/202510262346342_AgregarFechaEntregaPedidoMuestra.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Entity.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class AgregarFechaEntregaPedidoMuestra : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(@"IF NOT EXISTS (SELECT 1 FROM sys.columns WHERE Name = 'FechaEntrega' AND Object_ID = Object_ID('dbo.PedidoMuestra'))
+BEGIN
+    ALTER TABLE dbo.PedidoMuestra ADD FechaEntrega DATETIME NULL;
+END");
+        }
+
+        public override void Down()
+        {
+            Sql(@"IF EXISTS (SELECT 1 FROM sys.columns WHERE Name = 'FechaEntrega' AND Object_ID = Object_ID('dbo.PedidoMuestra'))
+BEGIN
+    ALTER TABLE dbo.PedidoMuestra DROP COLUMN FechaEntrega;
+END");
+        }
+    }
+}
